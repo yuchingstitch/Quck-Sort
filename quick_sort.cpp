0a822b39367a23440fcdc5fe568199a3f2524986
yuching kuo <yuchingstitch@gmail.com>
2023-05-11 18:05:14 +0800

    sixth upload (could be working)

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,64 +1,40 @@
 #include<iostream>
 using namespace std;
 
-int divide(int data[], int start, int end) {
-	int pivot = data[start]; //設定基準點
-	int pivotIndex = start;
+void quickSort(int data[], int left, int right) {
+    if (left >= right) {
+        return;
+    }
+	int pivot = data[left]; //設定基準點
+	int i = left;
+    int j = right;
 
-    cout << "in dividing" << endl;  // debug
-    while (start != end){
-        while(pivot >= data[start]){
-            start ++;
-            cout << "start = " << start << endl; // debug
-            if(start == end){
-                cout << "return 1"; //debug
-                
-            }
+    while (i != j){
+        while(pivot < data[j] && i < j){
+            j --;
         }
-        while(pivot < data[end]){
-            end --;
-            cout << "end = " << end << endl;
-            if(start == end){
-                cout << "return 2"; //debug
-                
-            }
+        while(pivot >= data[i] && i < j){
+            i ++;
         }
-
-        swap(data[start], data[end]);
-
-        for (int i = 0; i < 10; i++) { // debug
-		    cout << data[i] << " ";
+        if (i < j){
+            swap(data[i], data[j]);
+        }
+        for (int k = 0; k < 10; k++) { // debug
+		    cout << data[k] << " ";
 	    }
         cout << endl;
         cout << "-----------------------------------------------" << endl;
-        
     }
-    swap(data[pivotIndex],data[start]);
-    pivotIndex = start;
-    return pivotIndex;
-}
-
-void quickSort(int data[], int start, int end) {
-    cout << "in quickSort" << endl;
-	if (start >= end) {
-        return;
-    }
-	int p = divide(data, start, end); //一輪後回傳切割點
-    cout << "returned to quickSort" << endl;
-	//quickSort(data, start, p - 1); //呼叫自己處理左半邊
-	//quickSort(data, p + 1, end); //呼叫自己處理右半邊
+    data[left] = data[i];
+    data[i] = pivot;
+    cout << "end of round" << endl;
+    quickSort(data, left, i - 1); //呼叫自己處理左半邊
+	quickSort(data, i + 1, right); //呼叫自己處理右半邊
 }
 
 int main() {
     int data[] = {33, 67, 8, 13, 54, 119, 3, 84, 25, 41};
     int n = 10;
-    cout << "start" << endl;
-
     quickSort(data, 0, n - 1); //資料送入快速排序函式
-
-	for (int i = 0; i < n; i++) {
-		cout << data[i] << " ";
-	}
-
 	return 0;
 }
